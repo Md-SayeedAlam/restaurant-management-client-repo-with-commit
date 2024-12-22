59d357cbdc5d121de667662e316f8148284daa91
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-22 22:17:49 +0600

    added lottie react animations for loading

--- src/Components/PrivateRoute/PrivateRoute.jsx
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -2,15 +2,27 @@ import React, { useContext } from 'react';
 
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
-
+import Lottie from 'lottie-react';
+import lottieLoading from '../../assets/lottie/loading.json'
 const PrivateRoute = ({children}) => {
 
     const location = useLocation()
     console.log(location)
     const {user,loading} = useContext(AuthContext)
 
-    if(loading){
-        return <div className='flex justify-center items-center h-[200px]'><span className="loading loading-spinner size-20  text-accent"></span></div>
+    // if(loading){
+    //     return(
+    //         <Lottie animationData={lottieLoading}></Lottie>
+    //     )
+    // }
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-[200px]">
+                <Lottie animationData={lottieLoading} />
+                {/* <span className="loading loading-spinner size-20 text-accent"></span> */}
+                <h2 className='text-green-600 font-bold text-5xl'>Loading</h2>
+            </div>
+        );
     }
 
     if(user && user?.email) {return children}
