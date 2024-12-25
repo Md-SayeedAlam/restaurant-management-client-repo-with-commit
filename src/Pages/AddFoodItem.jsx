6517d140cbf57c2f34c38c5c6a27113ea194278a
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-25 14:27:48 +0600

    import useEffect in addFoodItem Page

--- src/Pages/AddFoodItem.jsx
+++ src/Pages/AddFoodItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../Components/AuthProvider/AuthProvider';
 import Swal from 'sweetalert2';
 
